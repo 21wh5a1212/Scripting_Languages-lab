12ac115ef30e31b8e031c3aa0c3d77f8fd8778b1
Abhinaya <109090776+21wh5a1210@users.noreply.github.com>
2023-03-16 18:49:21 +0530

    Create path.rb

---
+++ path.rb
@@ -0,0 +1,14 @@
+# file name
+fbname = File.basename "C:/home/it/21wh5a1210/path.rb"
+puts "File name: "+fbname
+
+# basename
+bname = File.basename "C:/home/it/21wh5a1210/path.rb",".rb"
+puts "Base name: "+bname
+
+# file extention
+ffextn = File.extname  "C:/home/it/21wh5a1210/path.rb"  
+puts "Extention: "+ffextn
+# path name
+path_name= File.dirname  "C:/home/it/21wh5a1210/path.rb"
+puts "Path name: "+path_name
