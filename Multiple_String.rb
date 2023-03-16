71ce946be0bb6175f570d2d5621845beafd3ad27
Abhinaya <109090776+21wh5a1210@users.noreply.github.com>
2023-03-16 18:52:05 +0530

    Create Multiple_String.rb

---
+++ Multiple_String.rb
@@ -0,0 +1,7 @@
+def multiple_string(str, n)
+    return str*n
+end
+print multiple_string('a', 1),"\n"
+print multiple_string('b', 2),"\n"
+print multiple_string('h', 3),"\n"
+print multiple_string('i', 4),"\n"
