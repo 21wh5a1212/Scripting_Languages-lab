2d4fba35fc009144a3438de9bf675537a5541857
Abhinaya <109090776+21wh5a1210@users.noreply.github.com>
2023-03-16 19:04:18 +0530

    Create integer.rb

---
+++ integer.rb
@@ -0,0 +1,9 @@
+def makes20(x,y)
+    return x == 20 || y == 20 || x + y == 20
+end
+
+print makes20(20, 30),"\n"
+print makes20(33, 22),"\n"
+print makes20(20, 20),"\n"
+print makes20(20, 55),"\n"
+print makes20(90, 44),"\n"
